a03b7c691635284612bdd929cc3576240cb4f002
chadseale <chad.seale@gmail.com>
2017-03-26 18:31:35 -0600

    Update Solver.java

--- src/puzzle/Solver.java
+++ src/puzzle/Solver.java
@@ -5,30 +5,35 @@ import edu.princeton.cs.algs4.Stack;
 import edu.princeton.cs.introcs.In;
 import edu.princeton.cs.introcs.StdOut;
 
-public class Solver{
+public class Solver {
+	
 	private Stack<Board> solution;
 	private int moves;
 	
 	// find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
     	
-    	if (initial.isSolvable() == false) throw new IllegalArgumentException();
+    	if (initial.isSolvable() == false) {
+    		throw new IllegalArgumentException();
+    	}
+    	
 		solution = new Stack<>();
 		MinPQ<Move> q = new MinPQ<>();
 		q.insert(new Move(initial, 0, null));
+		
 		while(true){
 			Move move = q.delMin();
 			if(move.board.isGoal()){ //goal has been reached, populate fields of interest and return
 				this.moves = move.moves;
 				do{
 					solution.push(move.board);
-					move = move.parent;
+					move = move.mainBoard;
 				}
 				while(move != null);
 				return; //done solving
 			}
 			for(Board next : move.board.neighbors()){
-				if(move.parent == null || !next.equals(move.parent.board)) //look back one move to prevent useless looping
+				if(move.mainBoard == null || !next.equals(move.mainBoard.board)) //look back one move to prevent useless looping
 					q.insert(new Move(next, move.moves+1, move));
 			}
 		}
@@ -52,19 +57,25 @@ public class Solver{
     private class Move implements Comparable<Move>{
 		private Board board;
 		private int moves;
-		private Move parent;
+		private Move mainBoard;
 		
-		public Move(Board board, int moves, Move parent){
+		public Move(Board board, int moves, Move mainBoard){
 			this.board = board;
 			this.moves = moves;
-			this.parent = parent;
+			this.mainBoard = mainBoard;
 		}
 
 		@Override
 		public int compareTo(Move o) {
 			int difference = this.board.manhattan() + this.moves - o.board.manhattan() - o.moves;
-			if (difference != 0) return difference; //return normal difference of priority functions
-			if (this.moves > o.moves) return -1; //if priority is the same give preference to the one with more moves
+			
+			if (difference != 0) {
+				return difference;  //return normal difference of priority functions
+			}
+			
+			if (this.moves > o.moves) {
+				return -1; //if priority is the same give preference to the one with more moves
+			}
 			return 1;
 		}
 	}
@@ -76,9 +87,13 @@ public class Solver{
         In in = new In(args[0]);
         int N = in.readInt();
         int[][] blocks = new int[N][N];
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++)
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 blocks[i][j] = in.readInt();
+            }
+        }
+        
         Board initial = new Board(blocks);
 
         // check if puzzle is solvable; if so, solve it and output solution
@@ -96,4 +111,3 @@ public class Solver{
         
     }
 }
-
